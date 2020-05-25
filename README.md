27ff02a852e41354bc71b9d9e0f838d8c7add6e7
DhrutiShah2412 <dhruti.shah241290@gmail.com>
2020-05-25 16:28:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5z9shj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5z9shj)
---
+++ src/app/app.component.ts
@@ -0,0 +1,89 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from "@angular/google-maps";
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow;
+
+  zoom = 12;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    //mapTypeId: "hybrid",
+    maxZoom: 15,
+    minZoom: 8,
+  };
+  markers = [];
+  infoContent = "";
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition(x => {
+      this.center = {
+        lat: x.coords.latitude,
+        lng: x.coords.longitude
+      };
+      this.markers.push({
+        position: {
+          lat: x.coords.latitude,
+          lng: x.coords.longitude
+        },
+        label: {
+          color: "blue",
+          text: "Marker Label"
+        },
+        title: "Marker Title",
+        info: "Marker info",
+        options: {
+          animation: google.maps.Animation.BOUNCE
+        }
+      });
+    });
+  }
+  openInfo(marker: MapMarker, info) {
+    this.infoContent = info;
+     this.info.open(marker);
+  
+ }
+  addMarker() {
+    this.markers.push({
+      position: {
+        lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
+        lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10,
+      },
+      label: {
+        color: 'red',
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+      info: 'Marker info ' + (this.markers.length + 1),
+      options: {
+        animation: google.maps.Animation.DROP,
+      },
+    });
+  }
+
+   zoomIn() {
+    if (this.zoom < this.options.maxZoom) this.zoom++;
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom) this.zoom--;
+  }
+
+  click(event: google.maps.MouseEvent) {
+    console.log(event);
+  }
+
+  logCenter() {
+    console.log(JSON.stringify(this.map.getCenter()));
+  }
+}
